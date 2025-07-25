9440cb0f7efc9b6d5e8154f6d34d8c2ab8bcf3ae
Léo <lbassani849@gmail.com>
2025-07-25 20:05:20 -0300

    Adding classes generated from StarUML

---
+++ code/game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Class3 import Class3
+
+
+class Game(Class3):
+    def __init__(self):
+        self.window = None
+
+    def run(self, ):
+        pass
---
+++ code/level.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from Entity import Entity
+from Entity import Entity
+
+
+class Level(Entity, Entity):
+    def __init__(self):
+        self.window = None
+        self.name = None
+        self.entity_list = None
+
+    def run(self, ):
+        pass
